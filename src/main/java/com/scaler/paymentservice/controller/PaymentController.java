aa7bc66ca9d331649b0ed355ce7b455a024d46e5
sai ram <sairam20929@gmail.com>
2024-05-03 23:35:07 +0530

    created dto

---
+++ src/main/java/com/scaler/paymentservice/controller/PaymentController.java
@@ -0,0 +1,7 @@
+package com.scaler.paymentservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+}
---
+++ src/main/java/com/scaler/paymentservice/dtos/InititatePaymentDto.java
@@ -0,0 +1,13 @@
+package com.scaler.paymentservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class InititatePaymentDto {
+    private String orderId;
+    private String email;
+    private String phoneNumber;
+    private Long amount;
+}
